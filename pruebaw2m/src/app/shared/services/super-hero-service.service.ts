2c81e2ceb0daef6a3dae3c0a9a22f6c1ea00c29b
Alberto Pulido Gómez <apulido@atsistemas.com>
2021-06-14 17:40:02 +0200

    finish day

--- pruebaw2m/src/app/shared/services/super-hero-service.service.ts
+++ pruebaw2m/src/app/shared/services/super-hero-service.service.ts
@@ -62,19 +62,18 @@ export class SuperHeroService {
   }
 
   deleteSuperHero(id:number){
-    const suscription = this.superHeros$.pipe(
-      filter(heroes => heroes.length > 0),
-      switchMap(heroes => new Observable<SuperHero[]>(obs => obs.next(heroes.filter(hero => hero.id !== id)))),
-      tap(heroes => localStorage.setItem('superheros', JSON.stringify(heroes)))
-    ).subscribe(resp => console.log(resp));
+    // const suscription = this.superHeros$.pipe(
+    //   filter(heroes => heroes.length > 0),
+    //   switchMap(heroes => new Observable<SuperHero[]>(obs => obs.next(heroes.filter(hero => hero.id !== id)))),
+    //   tap(heroes => console.log(heroes))
+    // ).subscribe(resp => console.log(resp));
 
-    // let newState : Array<SuperHero>;
-    // this.superHeros$.pipe(
-    //   tap(heroes => localStorage.setItem('superheros', JSON.stringify(heroes)))
-    // ).subscribe(
-    //   resp => newState = resp
-    // );
-    // newState = newState.filter(e => e.id!==id);
-    // this._superHeros$.next(newState);
+    let newState : Array<SuperHero>;
+    this.superHeros$.subscribe(
+      resp => newState = resp
+    );
+    newState = newState.filter(e => e.id!==id);
+    this._superHeros$.next(newState);
+    localStorage.setItem('superheros', JSON.stringify(newState))
   }
 }
